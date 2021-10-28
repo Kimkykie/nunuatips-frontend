7680e29e16fc35c27041efd3c592625a8058e719
kimkykie <kimkiragu@gmail.com>
2021-10-28 14:48:03 +0300

    update: add global css styles and header import to Page

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,55 @@
 import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'Lato', sans-serif;
+    padding: 0;
+    margin: 0;
+    line-height:2;
+  }
+  a {
+    text-decoration: none;
+    color: var(---black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: 'Lato', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default function Page({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <Header />
+      <InnerStyles>{children}</InnerStyles>
+    </div>
+  );
 }
 
 Page.propTypes = {
