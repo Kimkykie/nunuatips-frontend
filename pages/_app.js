af80cbc64e34398f0202d7c8d04b415c99c77ac8
kimkykie <kimkiragu@gmail.com>
2021-11-08 15:24:30 +0300

    feature: add nprogress for route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import Page from '../components/Page';
+import Router from 'next/router';
 import '../styles/global.css';
+import NProgress from 'nprogress';
+import Page from '../components/Page';
+
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
